5a1e41d2d5f432507d7ec1061349794ed8ccb654
Denise Henderson <denise@henderson3.us>
2020-07-22 18:06:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,14 +2,11 @@ class Dog
 
   def initialize (name, breed)
     @name = name
-  end
-
-  def breed
-    #if breed != nil
+    if breed != nil
       @breed = breed
-    #else
-    #  @breed = "Mutt"
-    #end
+    else
+      @breed = "Mutt"
+    end
   end
 
 end
